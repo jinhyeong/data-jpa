bf3450828c163a2926a542dbd707bdb5612ca9e5
jinhyeong <jinhyeong.b@gmail.com>
2022-05-25 22:36:01 +0900

    섹션 2. 예제 도메인 모델
    예제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,24 +1,40 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = { "id", "username", "age" }) // 연관관계 필드는 toString 하지 않는게 좋아
 public class Member {
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
+	@Column(name = "member_id")
 	private Long id;
 	private String username;
+	private int age;
 
-	protected Member() {
-	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id") // 외래키명
+	private Team team;
 
 	public Member(String username) {
 		this.username = username;
+	}
+
+	public Member(String username, int age, Team team) {
+		this.username = username;
+		this.age = age;
+		if (team !=null) {
+			changeTeam(team);
+		}
+	}
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
 	}
 }
---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,51 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberTest {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Test
+	void test() {
+		// given
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		entityManager.persist(teamA);
+		entityManager.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+
+		entityManager.persist(member1);
+		entityManager.persist(member2);
+		entityManager.persist(member3);
+		entityManager.persist(member4);
+
+		entityManager.flush();
+		entityManager.clear();
+		// when
+
+		// then
+		List<Member> members = entityManager.createQuery("select m from Member m", Member.class).getResultList();
+		members.forEach(member -> {
+			System.out.println("member = " + member);
+			System.out.println("member.getTeam = " + member.getTeam());
+		});
+	}
+}
